5e448fa01ae101c3913baf3f0850c8ae0cc2bc35
mazeiomli <mazeiomli@gmail.com>
2020-06-28 09:26:23 -0400

    install npm viz.js, python-shell. add java-src with linked list class

---
+++ webview-sample/java-src/LinkedListNode.java
@@ -0,0 +1,30 @@
+public class LinkedListNode{
+
+    private int data;
+    private LinkedListNode next;
+
+    LinkedListNode(int i){
+        setData(i);
+        setNext(null);
+    }
+    LinkedListNode(int i, LinkedListNode nxt){
+        setData(i);
+        setNext(nxt);
+    }
+
+    public int getData(){
+        return this.data;
+    }
+    public LinkedListNode getNext(){
+        return this.next;
+    }
+
+    public void setData(int data){
+        this.data = data;
+
+    }
+    public void setNext(LinkedListNode next){
+        this.next = next;
+    }
+
+}
