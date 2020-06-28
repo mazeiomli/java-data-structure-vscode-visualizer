0b775dc0f045677a6254d9373bde187382bba9d9
mazeiomli <mazeiomli@gmail.com>
2020-06-28 12:25:09 -0400

    create convert data struc to json java method

--- java-datastruc-vscode-viz/src/main/java/javaSrc/ConvertGraphlikeToJSON.java
+++ java-datastruc-vscode-viz/src/main/java/javaSrc/ConvertGraphlikeToJSON.java
@@ -5,19 +5,59 @@ string, which is written to disk.
 
 */
 
-
 package javaSrc;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class ConvertGraphlikeToJSON{
 
-	// TODO
-	public static void writeJSONStringToDisk(String fileName, String json){
-		return;
+	// maybe todo?
+	public static void convertLinkedListToJSON(LinkedListNode head, String filename){
+		try{
+			linkedListToJSON(head, filename);
+		}
+		catch(IOException e){
+			System.out.println(e.getMessage());
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
 
 	}
 
-	//todo
-	public static void linkedListToJSON(LinkedListNode head){
-		return;
+
+
+	//writes json rep of string to disk
+	public static void linkedListToJSON(LinkedListNode head, String filename) throws Exception{
+		JSONObject linkedListJSON = new JSONObject();
+		LinkedListNode curr = head;
+		//iterate through linked list, construct adj list
+		while(curr != null){
+			//add to adjlist
+			JSONObject nodeData = new JSONObject();
+			nodeData.put("data", curr.getData());
+			JSONArray adjList = new JSONArray();
+			if(curr.getNext() != null){
+				adjList.add(curr.getNext().getUID());
+			}
+			else{
+				adjList.add("null");
+			}
+			//forgot this line
+			nodeData.put("adjList", adjList);
+			// issue: put wrong variable
+			linkedListJSON.put(curr.getUID(), nodeData);
+
+			curr = curr.getNext();
+
+		}
+		// System.out.println(linkedListJSON.toJSONString());
+		Files.write(Paths.get(filename), linkedListJSON.toJSONString().getBytes());
 	}
 
 	
--- java-datastruc-vscode-viz/src/main/java/javaSrc/Runner.java
+++ java-datastruc-vscode-viz/src/main/java/javaSrc/Runner.java
@@ -5,6 +5,7 @@ and runs some code on it.
 
 package javaSrc;
 
+
 public class Runner{
 
 	public static LinkedListNode modifyLinkedList(LinkedListNode head){
@@ -18,8 +19,13 @@ public class Runner{
         LinkedListNode n2 = new LinkedListNode("n2", 2, n3);
 		LinkedListNode head = new LinkedListNode("n1", 1, n2);
 		//visualize
+		//convert data struc to json
+		ConvertGraphlikeToJSON.convertLinkedListToJSON(head, "before-data-struc.json");
+		
 		LinkedListNode newHead = modifyLinkedList(head);
 		//visualize
+		ConvertGraphlikeToJSON.convertLinkedListToJSON(newHead, "after-data-struc.json");
+
 		// should print 2
 		System.out.println(newHead.getData());
 	}
