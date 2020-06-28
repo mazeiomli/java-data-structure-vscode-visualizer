dafe235464fef8279a5f5c077663a933d6e60ec8
mazeiomli <mazeiomli@gmail.com>
2020-06-28 11:27:07 -0400

    add extension code to java-datastruc-vscode-viz dir

---
+++ java-datastruc-vscode-viz/javaSrc/ConvertGraphlikeToJSON.java
@@ -0,0 +1,24 @@
+/*
+Takes a graph-like data strucutre and converts it to an adjacency list
+representation. Then converts adj list representation to json
+string, which is written to disk.
+
+*/
+
+
+package javaSrc;
+public class ConvertGraphlikeToJSON{
+
+	// TODO
+	public static void writeJSONStringToDisk(String fileName, String json){
+		return;
+
+	}
+
+	//todo
+	public static void linkedListToJSON(LinkedListNode head){
+		return;
+	}
+
+	
+}
